21ae6163e8186293d4f7f3c38ab6993e28ee90ed
Nirob Hasan <nirob427417@gmail.com>
2023-12-01 22:42:08 +0600

    project setup

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <p className="text-4xl text-center">this is home page</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/types/common.ts
@@ -0,0 +1,39 @@
+export interface IMeta {
+  limit: number;
+  page: number;
+  total: number;
+}
+
+export type ResponseSuccessType = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any;
+  meta?: IMeta;
+};
+
+export type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
+
+export type IGenericErrorMessage = {
+  path: string | number;
+  message: string;
+};
+
+export interface Name {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+}
+
+export interface IUser {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  avatar: string;
+  domain: string;
+  available: boolean;
+}
